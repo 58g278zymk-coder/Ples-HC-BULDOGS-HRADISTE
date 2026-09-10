6c5303d0e73aa26c1b00d893c59fccd5027a9670
58g278zymk-coder <58g278zymk@privaterelay.appleid.com>
2026-09-10 20:39:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("HC BULDOGS HRADIŠTĚ");
+function openPhoto(src) {
+
+    const lightbox =
+        document.getElementById("lightbox");
+
+    const image =
+        document.getElementById("lightbox-img");
+
+    image.src = src;
+
+    lightbox.style.display = "flex";
+}
+
+
+function closePhoto() {
+
+    document.getElementById("lightbox")
+        .style.display = "none";
+}
+
+
+document.addEventListener(
+    "keydown",
+    function(event) {
+
+        if (event.key === "Escape") {
+
+            closePhoto();
+
+        }
+    }
+);
